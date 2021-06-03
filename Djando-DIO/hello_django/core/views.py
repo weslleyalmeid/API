23b03e3ac644ede49e2c428bd46e49aa833692f0
Weslley Almeida <weslley_fac@hotmail.com>
2021-06-03 17:59:46 -0300

    Create project agenda

--- Djando-DIO/hello_django/core/views.py
+++ Djando-DIO/hello_django/core/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome}, você tem  {idade} anos</h1>')
+
+
+def soma(request, a, b):
+    return HttpResponse(f'Soma de {a} + {b} = {a + b}')
+
+
+def multiplicacao(request, a, b):
+    return HttpResponse(f'Soma de {a} * {b} = {a * b}')
+
+
+def divisao(request, a, b):
+    return HttpResponse(f'Soma de {a} / {b} = {a / b}')
+
+
+def subtracao(request, a, b):
+    return HttpResponse(f'Soma de {a} - {b} = {a - b}')
