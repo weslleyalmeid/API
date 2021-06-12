7026ca0d3e9d081e589f8a87b1cb9ba3e0a3d21e
Weslley Almeida <weslley_fac@hotmail.com>
2021-06-12 09:05:37 -0300

    update, delete and filter

--- Djando-DIO/agenda/core/views.py
+++ Djando-DIO/agenda/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 # Create your views here.
 
@@ -88,9 +90,21 @@ def submit_evento(request):
 @ login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
 
     if usuario == evento.usuario:
         evento.delete()
-
+    else:
+        raise Http404()
     return redirect('/')
+
+
+# @ login_required(login_url='/login/')
+def json_lista_evento(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
